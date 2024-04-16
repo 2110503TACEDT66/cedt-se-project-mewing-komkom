5c477b3bd2e44b6ea5d61feb20192378979fc7cc
Nonnyss <nonthapan.wong@gmail.com>
2024-04-16 17:53:52 +0700

    Finish admin page

--- Frontend/src/app/adminpage/page.tsx
+++ Frontend/src/app/adminpage/page.tsx
@@ -2,19 +2,27 @@ import PreviewCard from "@/components/admin/PreviewCard";
 import React from "react";
 
 export default function page() {
+  const data = [1, 2, 3, 4, 5, 6, 7];
   return (
     <div>
-      <div className="flex justify-center mt-20 mb-10 text-5xl font-bold">
+      <div className="flex justify-center mt-20 mb-5 text-5xl font-bold">
         <div>
-          Manage{" "}
           <span className="bg-gradient-to-b from-blue-700 to-blue-400 bg-clip-text text-transparent">
-            Co-working{" "}
+            Manage Co-working Space
           </span>
-          Space
         </div>
       </div>
       <div>
-        <PreviewCard name="test" desc="test" img="test" seat="test" />
+        <div className="flex justify-center mb-5">
+          <button className="align-middle select-none text-lg font-semibold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none py-2 px-12 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
+            เพิ่ม
+          </button>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 justify-items-center">
+        {data.map((i: any) => (
+          <PreviewCard key={i} />
+        ))}
       </div>
     </div>
   );
--- Frontend/src/components/admin/PreviewCard.tsx
+++ Frontend/src/components/admin/PreviewCard.tsx
@@ -1,40 +1,69 @@
 import React from "react";
+import { MdOutlinePhotoLibrary } from "react-icons/md";
+import { AiFillClockCircle } from "react-icons/ai";
+import { Time } from "../../../interface";
 
 export default function PreviewCard({
   img,
   name,
   desc,
   seat,
+  time,
 }: {
-  img: string;
-  name: string;
-  desc: string;
-  seat: string;
+  img?: string;
+  name?: string;
+  desc?: string;
+  seat?: string;
+  time?: Time | null;
 }) {
-  return <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-  <div
-    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-    <img
-      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-      alt="card-image" />
-  </div>
-  <div className="p-6">
-    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-      UI/UX Review Check
-    </h5>
-    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-      The place is close to Barceloneta Beach and bus stop just 2 min by walk
-      and near to "Naviglio" where you can enjoy the main night life in
-      Barcelona.
-    </p>
-  </div>
-  <div className="p-6 pt-0">
-    <button
-      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-      type="button">
-      Read More
-    </button>
-  </div>
-</div>  
-;
+  return (
+    <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 p-3">
+      {img ? (
+        <div className="relative h-48 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+          <img src="" alt="card-image" />
+        </div>
+      ) : (
+        <div className="h-48 flex justify-center items-center shadow-lg bg-slate-200 bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40 overflow-hidden">
+          <MdOutlinePhotoLibrary size={80} color="slate" />
+        </div>
+      )}
+
+      <div className="p-4">
+        <div></div>
+        <h5 className="block text-xl antialiased font-semibold leading-snug tracking-normal text-black mt-1">
+          {name ? name : "ชื่อ"}
+        </h5>
+        <div className="flex gap-2">
+          <AiFillClockCircle className="mb-2" color="black" size={20} />
+          <div>
+            {time ? (
+              <div> {`${time.start} - ${time.end}`} </div>
+            ) : (
+              "เวลาเปิด - เวลาปิด"
+            )}
+          </div>
+        </div>
+        <hr />
+        <div className=" mt-2 text-lg font-medium text-black">รายละเอียด</div>
+        <div className="block  text-base antialiased font-light leading-relaxed text-inherit">
+          {desc ? (
+            desc
+          ) : (
+            <div className="text-xl">
+              . <br />. <br /> . <br />
+            </div>
+          )}
+        </div>
+        <div className="mt-2">จำนวนที่นั่ง : {seat}</div>
+      </div>
+
+      <div className=" flex justify-end ">
+        <button
+          className="align-middle select-none text-base font-normal text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none py-1 px-7 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+        >
+          แก้ไข
+        </button>
+      </div>
+    </div>
+  );
 }
