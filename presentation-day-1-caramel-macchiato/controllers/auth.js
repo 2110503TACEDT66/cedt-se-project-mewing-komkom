16173a94c989bc1e67f237baf6db4911e6208e14
namprungs <kuyyamasdfg@gmail.com>
2024-04-16 16:29:28 +0700

    feat:editUser

--- presentation-day-1-caramel-macchiato/controllers/auth.js
+++ presentation-day-1-caramel-macchiato/controllers/auth.js
@@ -94,6 +94,25 @@ exports.getMe = async (req, res, next) => {
   });
 };
 
+exports.EditUser = async(req,res,next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(400).json({ success: false });
+    }
+  
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    return res.status(400).json({
+      success: false
+    });
+  }
+
+}
+
 exports.logout = async (req, res, next) => {
   res.cookie("token", "none", {
     expires: new Date(Date.now() + 10 * 1000),
