f27a7187e679a124ca3ff4e18536c44f5dc91541
TENet1010 <Nattakorn2713@gmail.com>
2024-04-17 00:29:40 +0700

    add route delete user by id and fix authorize

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { register, login, getMe, logout, EditUser,getAllUser } = require("../controllers/auth");
+const {
+  register,
+  login,
+  getMe,
+  logout,
+  EditUser,
+  getAllUser,
+  deleteUser
+} = require("../controllers/auth");
 
 const router = express.Router();
 
@@ -9,6 +17,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 router.get("/logout", protect, logout);
-router.put("/edit/:id",protect,authorize("moderator"),EditUser);
-router.get("/alluser", protect, authorize("admin"), getAllUser);
+router.put("/edit/:id", protect, authorize("moderator"), EditUser);
+router.get("/alluser", protect, authorize("moderator"), getAllUser);
+router.delete("/deleleuser/:id", protect, authorize("moderator"),deleteUser);
 module.exports = router;
