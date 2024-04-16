c0b6588ecc3660e39d44f8f583b4ba2e4ccf59fd
Nonnyss <nonthapan.wong@gmail.com>
2024-04-16 22:07:25 +0700

    card and dashboard page

--- Frontend/src/app/adminpage/page.tsx
+++ Frontend/src/app/adminpage/page.tsx
@@ -1,11 +1,12 @@
 import PreviewCard from "@/components/admin/PreviewCard";
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
   const data = [1, 2, 3, 4, 5, 6, 7];
   return (
-    <div>
-      <div className="flex justify-center mt-20 mb-5 text-5xl font-bold">
+    <div className="p-10">
+      <div className="flex justify-center mt-10 mb-5 text-5xl font-bold">
         <div>
           <span className="bg-gradient-to-b from-blue-700 to-blue-400 bg-clip-text text-transparent">
             Manage Co-working Space
@@ -13,13 +14,16 @@ export default function page() {
         </div>
       </div>
       <div>
-        <div className="flex justify-center mb-5">
-          <button className="align-middle select-none text-lg font-semibold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none py-2 px-12 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
+        <div className="flex justify-center mb-10">
+          <Link
+            href="/adminpage/create"
+            className="align-middle select-none text-lg font-semibold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none py-2 px-12 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          >
             เพิ่ม
-          </button>
+          </Link>
         </div>
       </div>
-      <div className="grid grid-cols-4 justify-items-center">
+      <div className="grid grid-cols-4 justify-items-center gap-10">
         {data.map((i: any) => (
           <PreviewCard key={i} />
         ))}
