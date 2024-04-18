a0e20b453b9eb7341db3e7f118fe510538889d0e
Nonnyss <nonthapan.wong@gmail.com>
2024-04-18 16:02:40 +0700

    fix interface

--- Frontend/interface.ts
+++ Frontend/interface.ts
@@ -15,16 +15,16 @@ export interface RefWorkingSpace {
 }
 
 export interface SpaceItem {
-  _id: string;
+  _id?: string;
   name: string;
   address: string;
-  tel: string;
+  tel?: string;
   openTime: string;
   closeTime: string;
   remaining: number;
-  __v: number;
-  reservation: Reservation[];
-  id: string;
+  __v?: number;
+  reservation?: Reservation[];
+  id?: string;
   image: string;
 }
 
