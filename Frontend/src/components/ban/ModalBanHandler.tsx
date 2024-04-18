cc62f472e30d14b389c501352841e67e0b9442ab
RatchanonMo <49874@yupparaj.ac.th>
2024-04-18 15:55:49 +0700

    implement unban

--- Frontend/src/components/ban/ModalBanHandler.tsx
+++ Frontend/src/components/ban/ModalBanHandler.tsx
@@ -9,15 +9,25 @@ export default function ModalBanHandle({ data }: { data: any }) {
 
   return (
     <div className="flex gap-2 items-center mt-5">
-      <button
-        className="bg-rose-500 px-5 py-2 rounded-full text-white max-w-max "
-        onClick={() => {
-          setOpen(true);
-          console.log(open);
-        }}
-      >
-        Ban
-      </button>
+      {data.banUntil ? (
+        <button
+          className="bg-blue-500 px-5 py-2 rounded-full text-white max-w-max "
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Unban
+        </button>
+      ) : (
+        <button
+          className="bg-rose-500 px-5 py-2 rounded-full text-white max-w-max "
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Ban
+        </button>
+      )}
 
       <ModalBan isOpen={open} data={data} handleClose={() => setOpen(false)} />
     </div>
