647dc6893a3e6731453100d2f08ee772ccb9ad89
namprungs <kuyyamasdfg@gmail.com>
2024-04-19 04:34:09 +0700

    final editadmin

--- Frontend/src/components/moderator/editAdminForm.tsx
+++ Frontend/src/components/moderator/editAdminForm.tsx
@@ -13,22 +13,29 @@ export default function EditAdminForm({
   adminID: string;
   adminData: any;
 }) {
-    const [name,setName] = useState(adminData[0].name);
-    const [tel,setTel] = useState(adminData[0].tel);
-    const [email,setEmail] = useState(adminData[0].email);
-    const [role,setRole] = useState(adminData[0].role);
-
-    const handlerSave = async(e:any)=>{
-        try{
-            e.preventDefault();
-
-            const admin = await updateAdmin(adminID,name,email,tel,role,adminData.token);
-        }
-        catch(error){
-            console.log("Error for update" + error);
-        }
+  const [name, setName] = useState(adminData[0].name);
+  const [tel, setTel] = useState(adminData[0].tel);
+  const [email, setEmail] = useState(adminData[0].email);
+  const [role, setRole] = useState('admin');
+  const session = useSession();
+  const router = useRouter()
+  const handlerSave = async (e: any) => {
+    try {
+      e.preventDefault();
+      const admin = await updateAdmin(
+        adminID,
+        name,
+        email,
+        tel,
+        role,
+        session.data!.user.token
+      );
+      router.push('/moderatorpage')
+    } catch (error) {
+      console.log("Error for update" + error);
     }
-   
+  };
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <form
@@ -36,7 +43,9 @@ export default function EditAdminForm({
         className="flex flex-col justify-between p-5 bg-white w-[1042px] h-[588px] rounded-2xl shadow-2xl"
         onSubmit={handlerSave}
       >
-        <div className="text-center text-xl font-semibold text-gray-600 mt-3">Edit Admin</div>
+        <div className="text-center text-xl font-semibold text-gray-600 mt-3">
+          Edit Admin
+        </div>
 
         <div className="flex items-center">
           <label className="w-32">ชื่อ:</label>
@@ -46,7 +55,10 @@ export default function EditAdminForm({
             className="col-span-3"
             maxLength={25}
             id="admin_name"
-            onChange={(e)=>{console.log(e.target.value); setName(e.target.value)}}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setName(e.target.value);
+            }}
             value={name}
           />
         </div>
@@ -59,7 +71,10 @@ export default function EditAdminForm({
             className="col-span-3"
             maxLength={10}
             id="admin_tel"
-            onChange={(e)=>{console.log(e.target.value); setTel(e.target.value)}}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setTel(e.target.value);
+            }}
             value={tel}
           />
         </div>
@@ -72,17 +87,28 @@ export default function EditAdminForm({
             className="col-span-3"
             maxLength={25}
             id="admin_email"
-            onChange={(e)=>{console.log(e.target.value); setEmail(e.target.value)}}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setEmail(e.target.value);
+            }}
             value={email}
           />
         </div>
 
         <div className="flex items-center">
           <label className="w-32">Role:</label>
-          <select name="Role" id="role" className="col-span-3" onChange={(e)=>{console.log(e.target.value);setRole(e.target.value)}}>
+          <select
+            name="Role"
+            id="role"
+            className="col-span-3"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          >
             {" "}
-            <option value="Admin">Admin</option>
-            <option value="Moderator">Moderator</option>
+            <option value="admin">Admin</option>
+            <option value="moderator">Moderator</option>
           </select>
         </div>
 
@@ -90,8 +116,7 @@ export default function EditAdminForm({
           <button
             type="submit"
             className="rounded-md bg-sky-600 hover:bg-green-600 px-3 py-2
-                shadow-sm text-white w-[247px] h-[55px]" 
-                
+                shadow-sm text-white w-[247px] h-[55px]"
           >
             Save
           </button>
--- Frontend/src/libs/updateAdmin.tsx
+++ Frontend/src/libs/updateAdmin.tsx
@@ -2,48 +2,51 @@ import Swal from "sweetalert2";
 
 export default async function updateAdmin(
   id: string,
-  name:string,
-  email:string,
-  tel:string,
-  role:string,
+  name: string,
+  email: string,
+  tel: string,
+  role: string,
   token: string,
-
-
 ) {
-//   const body = JSON.stringify({ reserveDate: date }); // Construct the request body
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URI}/auth/edit/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          tel: tel,
+          role: role,
+        }),
+      }
+    );
 
-  const response = await fetch(
-   `${process.env.NEXT_PUBLIC_BACKEND_URI}/edit/${id}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        tel: tel,
-        role: role
+    if (!response.ok) {
+      const ans = await response.json();
+      Swal.fire({
+        title: "Error!",
+        text: ans.message || "Cannot Update #" + id,
+        icon: "error",
+      });
+    } else {
+      Swal.fire({
+        title: "Success!",
+        text: "Update successfully",
+        icon: "success",
+      });
 
-      }), // Include the request body
+      return await response.json();
     }
-  );
-  if (!response.ok) {
-    const ans = await response.json();
-
+  } catch (error) {
     Swal.fire({
       title: "Error!",
-      text: ans.message ||"Cannot Update #" + id,
+      text: (error as any).message || "Cannot Update #" + id,
       icon: "error",
     });
-  } else {
-    Swal.fire({
-      title: "Success!",
-      text: "Update successfully",
-      icon: "success",
-    });
-
-    return await response.json();
   }
 }
