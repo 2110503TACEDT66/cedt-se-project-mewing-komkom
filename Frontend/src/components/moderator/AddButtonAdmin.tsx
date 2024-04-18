2cfff5d26d6e05208e8d5fdab22378eff5a227cf
taksapornung <taksa.view@gmail.com>
2024-04-19 06:55:58 +0700

    add new admin page

--- Frontend/src/components/moderator/AddButtonAdmin.tsx
+++ Frontend/src/components/moderator/AddButtonAdmin.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 export default function AddButtonAdmin() {
     const router = useRouter();
   return (
-    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={(e)=>{router.push(`/moderator/add`);}}>
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={(e)=>{router.push(`/moderatorpage/add`);}}>
           +Add
         </button>
   )
