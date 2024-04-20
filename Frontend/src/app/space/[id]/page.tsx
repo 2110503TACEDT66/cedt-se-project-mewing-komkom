e9bc8115ce9960fb2712d4505d403c7f9b53998f
namprungs <kuyyamasdfg@gmail.com>
2024-04-20 16:30:56 +0700

    only design reservasion page

--- Frontend/src/app/space/[id]/page.tsx
+++ Frontend/src/app/space/[id]/page.tsx
@@ -8,7 +8,7 @@ import DateReserve from "@/components/DateReserve";
 import dayjs, { Dayjs } from "dayjs";
 import { useCardContext } from "@/context/CardContext";
 import { SpaceItem } from "../../../../interface";
-
+import { TimePickerProps } from "@mui/x-date-pickers";
 interface Props {
   params: { id: string };
 }
@@ -17,18 +17,27 @@ export default function SpaceDetail({ params }: Props) {
   const format = "HH:mm";
   const [date, setDate] = useState<Dayjs>();
   const [space, setSpace] = useState<SpaceItem>();
+  const [time, setTime] = useState<Dayjs | null>(null);
 
-  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+  const onChangeDate: DatePickerProps["onChange"] = (date, dateString) => {
     setDate(date);
     console.log(dateString);
   };
+
+  const onChangeTime = (time: Dayjs) => {
+    setTime(time);
+    console.log(time);
+  };
+
+  
+  
+  
 
   useEffect(() => {
     const fetchSpace = async () => {
       try {
         const spaceData = await getSpace(params.id);
         if (spaceData.data) {
-          console.log(spaceData.data);
           setSpace(spaceData.data);
         }
       } catch (error) {
@@ -41,7 +50,7 @@ export default function SpaceDetail({ params }: Props) {
 
   return (
     <div className="flex justify-center my-20">
-      <div className="bg-white relative flex justify-center items-center p-4 rounded-3xl w-[1184px] h-[613px]">
+      <div className="bg-white relative flex justify-center items-center p-4 pl-10 rounded-3xl w-[1184px] h-[613px]">
         <div
           className="bg-gray-200 w-[512px] h-[478px] rounded-2xl"
           style={{
@@ -74,7 +83,7 @@ export default function SpaceDetail({ params }: Props) {
             <div className="flex items-center gap-5 mb-3">
               <div className="text-[#736868] font-semibold text-base">Date</div>
               <div>
-                <DatePicker className="border-[#979797]" onChange={onChange} />
+                <DatePicker className="border-[#979797]" onChange={onChangeDate} />
               </div>
             </div>
             <div className="flex items-center">
@@ -86,7 +95,7 @@ export default function SpaceDetail({ params }: Props) {
                   format={format}
                   className="w-[150px] border-[#979797]"
                   id="inputTimeOpen"
-                  // onChange={}
+                  onChange={onChangeTime}
                   // defaultValue={dayjs(space.data?.openTime, 'HH:mm')}
                 />
                 <div className="text-center self-center text-[#736868] font-semibold text-base">
@@ -96,7 +105,7 @@ export default function SpaceDetail({ params }: Props) {
                   format={format}
                   className="w-[150px] border-[#979797] "
                   id="inputTimeClose"
-                  // onChange={}
+                  onChange={onChangeTime}
                   // defaultValue={dayjs(space.data?.closeTime, 'HH:mm')}
                 />
               </div>
