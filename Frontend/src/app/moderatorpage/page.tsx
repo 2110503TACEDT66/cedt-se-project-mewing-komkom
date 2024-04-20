bff17df76acdd41d68f75cba61cb29cc7cba7c05
taksapornung <taksa.view@gmail.com>
2024-04-20 19:34:58 +0700

    add search box for seaching list by role or name

--- Frontend/src/app/moderatorpage/page.tsx
+++ Frontend/src/app/moderatorpage/page.tsx
@@ -1,25 +1,43 @@
+'use client'
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import getAllAdmins from "@/libs/getallAdmins";
 import AdminItem from "@/components/moderator/AdminItem";
 import { User } from "../../../interface";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import AddButtonAdmin from "@/components/moderator/AddButtonAdmin";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import getAllUser from "@/libs/getUsers";
+
 
 interface Props{
   params: {id: string};
 }
 
-export default async function ModeratorPage() {
-  const session = await getServerSession(authOptions);
-  if (!session || !session.user.token) return null;
-  const allUsers_obeject = await getAllAdmins(session.user.token);
-  const usersdata = allUsers_obeject.data;
-
-
+export default function ModeratorPage() {
+  const session = useSession();
+  if (!session) return null;
+  // const allUsers_obeject = await getAllAdmins(session.user.token);
+  // const usersdata = allUsers_obeject.data;
+  const [users,setusers] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const userData = await getAllAdmins((session as any).data?.user.token);
+      if(!userData){
+        console.error("Invalid user data: ",userData);
+        return;
+      }
+      setusers(userData.data);
+    };
+    fetchData();
+  })
+  const [search, setSearch] = useState('');
+  const handleSearch = (event : any) => {
+    setSearch(event.target.value);
+  };
+  
   return (
     <div className="p-4"> {/* Add padding for spacing */}
       <div className="text-2xl font-bold mb-4">Manage Account</div>
@@ -31,8 +49,9 @@ export default async function ModeratorPage() {
           variant="outlined"
           size="small"
           fullWidth 
+          onChange={handleSearch}
         />
-        <AddButtonAdmin/>
+        {/* <AddButtonAdmin/> */}
       </Stack>
 
       {/* Table */}
@@ -56,9 +75,12 @@ export default async function ModeratorPage() {
           </thead>
           <tbody className="divide-y divide-gray-200">
             
-          {usersdata.map((eachadmin: User) => (
+          {users.filter((item:any)=>{return(item.name.includes(search)||item.role.includes(search))})
+          .map((eachadmin: User) => (
           <AdminItem admin={eachadmin} />
         ))}
+
+
           </tbody>
         </table>
       </div>
