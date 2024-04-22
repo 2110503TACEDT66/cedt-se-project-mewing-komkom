b6848b1f505cbefb651d7bc0764389aa776bcdbc
namprungs <kuyyamasdfg@gmail.com>
2024-04-22 21:07:06 +0700

    edit TimeSelectionComponent

--- Frontend/src/components/ui/TimeSelectionProps.tsx
+++ Frontend/src/components/ui/TimeSelectionProps.tsx
@@ -2,34 +2,25 @@ import { TimePicker } from "antd";
 import { Dayjs } from "dayjs";
 
 interface TimeSelectionProps {
-    handleTimeChange: (time: Dayjs, timeType: string) => void;
-    disabledTime: (current: Dayjs) => any;
-  }
-  
-  const TimeSelection: React.FC<TimeSelectionProps> = ({
-    handleTimeChange,
-    disabledTime,
-  }) => {
-    return (
-      <div className="col-span-3 flex gap-3">
-        <TimePicker
-          format="HH:mm"
-          className="w-[150px] border-[#979797]"
-          onChange={(time) => handleTimeChange(time, "start")}
-          minuteStep={30}
-        />
-        <div className="text-center self-center text-[#736868] font-semibold text-base">
-          To
-        </div>
-        <TimePicker
-          format="HH:mm"
-          className="w-[150px] border-[#979797] "
-          onChange={(time) => handleTimeChange(time, "end")}
-          minuteStep={30}
-          disabledTime={disabledTime}
-        />
-      </div>
-    );
-  };
+  handleTimeChange: (time: Dayjs, timeType: string) => void;
+  disabledTime: (current: Dayjs) => any;
+  typeTime:string
+}
 
-  export default TimeSelection;
+const TimeSelection: React.FC<TimeSelectionProps> = ({
+  handleTimeChange,
+  disabledTime,
+  typeTime
+}) => {
+  return (
+    <TimePicker
+      format="HH:mm"
+      className="w-[150px] border-[#979797] "
+      onChange={(time) => handleTimeChange(time, typeTime)}
+      minuteStep={30}
+      disabledTime={disabledTime}
+    />
+  );
+};
+
+export default TimeSelection;
