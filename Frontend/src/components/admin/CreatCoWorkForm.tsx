600e1d83fdb876f36d4a9b114d07f6274b868e97
Nonnyss <nonthapan.wong@gmail.com>
2024-04-22 11:30:10 +0700

    new use context state

---
+++ Frontend/src/components/admin/CreatCoWorkForm.tsx
@@ -0,0 +1,136 @@
+"use client";
+import React, { useState } from "react";
+import { TimePicker } from "antd";
+import dayjs from "dayjs";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "../ui/input";
+import { useCardContext } from "@/context/CardContext";
+import createCoWorkingSpace from "@/libs/createWorkingSpace";
+import { SetPreviewCard } from "../../../interface";
+import { useSession } from "next-auth/react";
+import updateWorkingSpace from "@/libs/updateWorkingSpace";
+
+interface Props {
+  data?: any;
+}
+
+export default function CoWorkForm({ data }: Props) {
+  const format = "HH:mm";
+  const {
+    handleFormChange,
+    handleOpenChange,
+    handleCloseChange,
+    amount,
+    inputValue,
+    isValid,
+    card,
+  } = useCardContext();
+
+  const session = useSession();
+  const onSubmit = (e: any) => {
+    createCoWorkingSpace(
+      {
+        name: card.name,
+        address: card.address,
+        tel: "081530",
+        openTime: card.openTime,
+        closeTime: card.closeTime,
+        remaining: card.remaining,
+        image: card.image,
+      },
+      session.data!.user.token
+    );
+  };
+
+  return (
+    <div className=" bg-white rounded-2xl shadow-2xl">
+      <form onSubmit={onSubmit} className="p-20 grid grid-cols-4 gap-10 ">
+        <label htmlFor="image-upload">รูปภาพ:</label>
+        {/* <Input
+            type="file"
+            className="col-span-1 max-w-60 text-gray-400"
+            id="image-upload"
+            name="image"
+            accept="image/*"
+            onChange={handleFileChange}
+          /> */}
+        <Input
+          required
+          type="text"
+          className="col-span-2 text-xs max-w-60 text-gray-400"
+          placeholder="Image Url"
+          id="image"
+          onChange={handleFormChange}
+          defaultValue={data?.image}
+        />
+        <div className="col-span-1"></div>
+
+        {/* <label className="cursor-pointer col-span-3">
+          <input type="file" className="hidden" />
+          <span className=" text-sm underline text-[#9CA3AF] hover:underline-offset-2 bg-[#E1E7EA] px-6 py-1 rounded-lg top-0">
+            Upload File
+          </span>
+        </label> */}
+        <label>ชื่อ:</label>
+        <Input
+          required
+          type="text"
+          placeholder="co-working space's name"
+          className="col-span-3 "
+          onChange={handleFormChange}
+          maxLength={25}
+          id="name"
+          defaultValue={data?.name}
+        />
+        <label>เวลาเปิด:</label>
+        <div className="col-span-3 flex gap-3">
+          <TimePicker
+            required
+            format={format}
+            className=" "
+            id="openTime"
+            onChange={handleOpenChange}
+            /* defaultValue={dayjs(data?.openTime, 'HH:mm')} */
+          />
+          <div className="text-center self-center">ถึง</div>
+          <TimePicker
+            required
+            format={format}
+            className=" "
+            id="closeTime"
+            onChange={handleCloseChange}
+            /* defaultValue={dayjs(data?.closeTime, 'HH:mm')} */
+          />
+        </div>
+        <label>รายละเอียด:</label>
+        <Textarea
+          required
+          className="col-span-3 "
+          placeholder="co-working space's detail"
+          onChange={handleFormChange}
+          maxLength={120}
+          id="address"
+          defaultValue={data?.address}
+        />
+        <label>จำนวนที่นั่ง:</label>
+
+        <Input
+          required
+          type="number"
+          placeholder="Max seats"
+          className=" rounded-lg p-3"
+          onChange={handleFormChange}
+          id="remaining"
+          min={1}
+        />
+
+        <div className="col-span-2"></div>
+        <div className="col-span-3"></div>
+        <Input
+          type="submit"
+          className="flex justify-center rounded-full text-md w-36 transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+        />
+      </form>
+    </div>
+  );
+}
