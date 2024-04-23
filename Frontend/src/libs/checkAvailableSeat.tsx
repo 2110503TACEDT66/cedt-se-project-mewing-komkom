cfda36cbffdc872a2e4d0659ba67a8888d2d8d31
namprungs <kuyyamasdfg@gmail.com>
2024-04-23 23:56:27 +0700

    add and edit some libs

---
+++ Frontend/src/libs/checkAvailableSeat.tsx
@@ -0,0 +1,20 @@
+export default async function checkAvailableSeat(data:any,id: String) {
+    
+  
+    const response = await fetch(
+     `${process.env.NEXT_PUBLIC_BACKEND_URI}/workingspace/available/${id}`,
+     {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+  
+    if (!response.ok) {
+      throw new Error("Failed to fetch space");
+    }
+  
+    return await response.json();
+  }
