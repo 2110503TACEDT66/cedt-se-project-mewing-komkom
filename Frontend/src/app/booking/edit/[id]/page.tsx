e74c8ad1e4f6a4bebb45f3bdafdef24b45326207
Nonnyss <nonthapan.wong@gmail.com>
2024-04-25 16:01:54 +0700

    init

--- Frontend/src/app/booking/edit/[id]/page.tsx
+++ Frontend/src/app/booking/edit/[id]/page.tsx
@@ -65,177 +65,6 @@ export default function ReservationDetail({ params }: Props) {
     fetchReserve();
   }, [params.id]);
 
-  /* useEffect(() => {
-    if (!date) {
-      setAvailableSeat("-");
-      return;
-    }
-    if (startTime && endTime) {
-      const fetchAvailable = async () => {
-        try {
-          const availableseatha = await checkAvailableSeat(
-            { startTime, endTime },
-            params.id
-          );
-          if (!availableseatha) {
-            throw new Error("cannot fetch");
-          }
-          setAvailableSeat(availableseatha.availableSeats);
-        } catch (error) {
-          console.error("error ja", error);
-        }
-      };
-      fetchAvailable();
-    }
-  }, [startTime, endTime, params.id, isReserve]); */
-
-  const handleDateChange: DatePickerProps["onChange"] = (date, dateString) => {
-    setDate(date);
-    if (date) {
-      setStartTime(
-        date.hour(startTime?.hour() || 0).minute(startTime?.minute() || 0)
-      );
-      setEndTime(
-        date.hour(endTime?.hour() || 0).minute(endTime?.minute() || 0)
-      );
-    }
-  };
-
-  const handleTimeChange = (time: Dayjs | null, timeType: string) => {
-    let timewithdate = date;
-    if (date === undefined) {
-      timewithdate = dayjs();
-    }
-    if (timewithdate) {
-      if (timeType === "start") {
-        setStartTime(
-          timewithdate.hour(time?.hour() || 0).minute(time?.minute() || 0) ||
-            timewithdate
-        );
-      } else if (timeType === "end") {
-        setEndTime(
-          timewithdate.hour(time?.hour() || 0).minute(time?.minute() || 0) ||
-            timewithdate
-        );
-      }
-    }
-  };
-
-  const disabledEndTime = (current: Dayjs) => {
-    let closeHour = dayjs(space?.closeTime).hour();
-    let closeMinute = dayjs(space?.closeTime).minute();
-    let currentTimeHour = dayjs().hour();
-    return {
-      disabledHours: () => {
-        if (!startTime) return Array.from({ length: 24 }, (_, i) => i);
-        if (!date) return Array.from({ length: 24 }, (_, i) => i);
-        let timeLength = startTime.hour();
-        if (startTime.minute() == 30) {
-          timeLength = timeLength + 1;
-        }
-        let arrayOfHours = Array.from(
-          { length: Math.max(0, timeLength) },
-          (_, i) => i
-        );
-        if (date?.date() === dayjs().date()) {
-          for (let i = 0; i < currentTimeHour; i++) {
-            arrayOfHours.push(i);
-          }
-        }
-        if (closeHour) {
-          for (let i = closeHour + 1; i < 24; i++) {
-            arrayOfHours.push(i);
-          }
-        }
-        return arrayOfHours;
-      },
-      disabledMinutes: (selectedHour: number) => {
-        if (!startTime) return Array.from({ length: 60 }, (_, i) => i);
-        if (!date) return Array.from({ length: 60 }, (_, i) => i);
-        const startHour = startTime.hour();
-        const startMinute = startTime.minute();
-
-        let arrayOfMinute = [];
-        if (selectedHour === startHour) {
-          for (let i = 0; i < startMinute + 1; i++) {
-            arrayOfMinute.push(i);
-          }
-        }
-
-        if (selectedHour === closeHour) {
-          for (let i = closeMinute + 1; i < 60; i++) {
-            arrayOfMinute.push(i);
-          }
-        }
-
-        if (selectedHour < startHour)
-          return Array.from({ length: 60 }, (_, i) => i);
-
-        return arrayOfMinute;
-      },
-    };
-  };
-
-  const disabledStartTime = (current: Dayjs) => {
-    let openHour = dayjs(space?.openTime).hour();
-    let openMinute = dayjs(space?.openTime).minute();
-    let closeHour = dayjs(space?.closeTime).hour();
-    let closeMinute = dayjs(space?.closeTime).minute();
-    return {
-      disabledHours: () => {
-        if (!dayjs(space?.openTime))
-          return Array.from({ length: 24 }, (_, i) => i);
-        if (!date) return Array.from({ length: 24 }, (_, i) => i);
-        let currentTimeHour = dayjs().hour();
-        let timeLength = openHour;
-        let arrayOfHours = Array.from(
-          { length: Math.max(0, timeLength) },
-          (_, i) => i
-        );
-        if (date?.date() === dayjs().date()) {
-          for (let i = 0; i < currentTimeHour; i++) {
-            arrayOfHours.push(i);
-          }
-        }
-        for (let i = closeHour + 1; i < 24; i++) {
-          arrayOfHours.push(i);
-        }
-        return arrayOfHours;
-      },
-      disabledMinutes: (selectedHour: number) => {
-        if (!dayjs(space?.openTime))
-          return Array.from({ length: 60 }, (_, i) => i);
-        if (!date) return Array.from({ length: 60 }, (_, i) => i);
-        let arrayOfMinute = [];
-        let currentTimeMinute = dayjs().minute();
-        let currentTimeHour = dayjs().hour();
-        if (date?.date() === dayjs().date()) {
-        }
-        if (currentTimeHour === selectedHour) {
-          for (let i = 0; i < currentTimeMinute; i++) {
-            arrayOfMinute.push(i);
-          }
-        }
-        if (selectedHour === openHour) {
-          for (let i = 0; i < Math.ceil(openMinute / 30); i++) {
-            arrayOfMinute.push(i * 30);
-          }
-        }
-        if (selectedHour === closeHour) {
-          for (let i = closeMinute; i < 60; i++) {
-            arrayOfMinute.push(i);
-          }
-        }
-
-        if (selectedHour < openHour) {
-          arrayOfMinute = Array.from({ length: 60 }, (_, i) => i);
-        }
-
-        return arrayOfMinute;
-      },
-    };
-  };
-
   return (
     <div className="flex justify-center my-20">
       <div className="bg-white relative flex justify-center items-center p-4 pl-10 rounded-3xl w-[1184px] h-[613px]">
@@ -272,11 +101,11 @@ export default function ReservationDetail({ params }: Props) {
             <div className="flex items-center gap-5 mb-3">
               <div className="text-[#736868] font-semibold text-base">Date</div>
               <div>
-                <DatePicker
+                {/*  <DatePicker
                   className="border-[#979797]"
                   onChange={handleDateChange}
                   value={date}
-                />
+                /> */}
               </div>
             </div>
             <div className="flex items-center">
@@ -284,19 +113,19 @@ export default function ReservationDetail({ params }: Props) {
                 Time
               </label>
               <div className="col-span-3 flex gap-3">
-                <TimeSelection
+                {/* <TimeSelection
                   handleTimeChange={handleTimeChange}
                   disabledTime={disabledStartTime}
                   typeTime="start"
-                />
+                /> */}
                 <div className="text-center self-center text-[#736868] font-semibold text-base">
                   To
                 </div>
-                <TimeSelection
+                {/* <TimeSelection
                   handleTimeChange={handleTimeChange}
                   disabledTime={disabledEndTime}
                   typeTime="end"
-                />
+                /> */}
               </div>
             </div>
             <div>Available seat : {availableSeat}</div>
