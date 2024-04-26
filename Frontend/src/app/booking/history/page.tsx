bf4fce6b00c61c5185ca6d189cfc1af9d4b7f6ac
TENet1010 <Nattakorn2713@gmail.com>
2024-04-27 02:22:58 +0700

    add some responsive and route product detail

---
+++ Frontend/src/app/booking/history/page.tsx
@@ -0,0 +1,23 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import getAllReservation from "@/libs/getallReserve";
+import { Reservation } from "../../../../interface";
+import ReservationItem from "@/components/Reservationitem";
+
+export default async function historyPage() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
+  const reservations = await getAllReservation(session.user.token);
+  const reservationData = reservations.data;
+  return (
+    <main>
+      <div>
+        <div className="text-3xl m-4">All Reservation</div>
+        {reservationData.map((reservation: Reservation) => (
+          <ReservationItem reservation={reservation} />
+        ))}
+      </div>
+    </main>
+  );
+}
