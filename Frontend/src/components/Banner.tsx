56abe74faa105d5fb90d0bd5e2d9ba005d50fcc9
Nonnyss <nonthapan.wong@gmail.com>
2024-04-27 17:45:31 +0700

    New fetch method @Homepage

--- Frontend/src/components/Banner.tsx
+++ Frontend/src/components/Banner.tsx
@@ -2,16 +2,27 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import getUserProfile from "@/libs/getUserProfile";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { useEffect } from "react";
 import Marquee from "react-fast-marquee";
 
-export default async function Banner() {
-  const session = await getServerSession(authOptions);
+export default function Banner() {
   let isAdmin: boolean | null = null;
-  if (session) {
-    const profile = await getUserProfile(session?.user.token);
-    const role = profile.data.role;
-    isAdmin = role === "admin" ? true : false;
-  }
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const session = await getServerSession(authOptions);
+        if (session) {
+          const profile = await getUserProfile(session?.user.token);
+          const role = profile.data.role;
+          isAdmin = role === "admin" ? true : false;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchSession();
+  }),
+    [];
 
   const imgList = [
     "https://www.brandbuffet.in.th/wp-content/uploads/2019/08/Samyan-COOP-1.jpg",
@@ -26,7 +37,7 @@ export default async function Banner() {
     <div className="pt-[130px] pb-40">
       <div className="flex flex-col justify-center gap-7 w-full">
         <span className="px-4 py-2 bg-blue-100 mx-auto max-w-max rounded-full text-blue-600">
-          {session && isAdmin
+          {isAdmin
             ? "Elevate this site towards success"
             : "Join us for productive vibes!"}
         </span>{" "}
@@ -34,13 +45,13 @@ export default async function Banner() {
           <span className="bg-gradient-to-b from-blue-700 to-blue-400 bg-clip-text text-transparent">
             Co-working{" "}
           </span>
-          {session && isAdmin ? "Space Manage System " : "Space Reservation"}
+          {isAdmin ? "Space Manage System " : "Space Reservation"}
         </h1>
         <p className="max-w-xl text-center mx-auto">
           Discover seamless co-working bookings. Reserve your space
           effortlessly."
         </p>
-        {session && isAdmin ? (
+        {isAdmin ? (
           <Link
             href="/adminpage"
             className="bg-black max-w-max text-white px-6 py-3 mx-auto rounded-full"
