12a2c0e6160232818f82e9295158fcd92c9edc61
namprungs <kuyyamasdfg@gmail.com>
2024-04-29 16:23:27 +0700

    cypress

--- Frontend/cypress/e2e/epic2.cy.ts
+++ Frontend/cypress/e2e/epic2.cy.ts
@@ -26,6 +26,7 @@ describe('Make reservation page', () => {
     cy.get('button[type="submit"]').click();
     cy.wait(2000);
     cy.visit('/space/662f51b9519d18088daacf91');
+    cy.wait(1000)
   });
 
   it('No date provided', () => {
@@ -51,4 +52,41 @@ describe('Make reservation page', () => {
   // it('successfully makes a reservation with valid data', () => {
     
   // });
+})
+
+//US2-3
+describe('edit reservation', () => {
+  beforeEach(() => {
+    // Visit the login page and login
+    // cy.visit('/space/6621e0dd0ca3b59b54586983');
+    cy.visit('/login');
+    cy.get('input[name="email"]').type('torrak@gmail.com');
+    cy.get('input[name="password"]').type('123456');
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+    cy.visit('/booking/manage');
+    cy.wait(1000)
+    cy.get('[data-testid="reservationtest"]').last().contains('Edit').click();
+    cy.wait(5000)
+  });
+
+  
+
+  it('Provide valid time', () => {
+    cy.get('[data-testid="spaceDatePicker"]').clear().type("2024-05-03").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').clear().type("10:30").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').clear().type("11:30").type('{enter}');
+    cy.wait(2000)
+    cy.contains('button', 'Save Change').click();
+    cy.contains('Update successfully').should('be.visible');
+  });
+
+  it('Provide invalid time', () => {
+    cy.get('[data-testid="spaceDatePicker"]').clear().type("2024-05-03").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').clear().type("11:30").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').get(".ant-picker-clear").last().click();
+    cy.wait(2000)
+    cy.contains('button', 'Save Change').click();
+    cy.contains('Cannot update Reservation').should('be.visible');
+  });
 })
