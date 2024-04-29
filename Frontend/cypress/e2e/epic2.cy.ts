a5586468de0bd3a4669292b8acb1b9fa08298d01
torrak <156417791+torrak@users.noreply.github.com>
2024-04-29 19:49:42 +0700

    Use cypress to test US2-1, US2-2

--- Frontend/cypress/e2e/epic2.cy.ts
+++ Frontend/cypress/e2e/epic2.cy.ts
@@ -1,35 +1,19 @@
 import { request } from "http"
 
-// describe('loginAndStoreToken', () => {
-//   it('Should login', () => {
-//     cy.request({
-//       method: 'POST',
-//       url: 'https://cedt-se-project-mewing-komkom-nq7g.vercel.app/api/v1/auth/login',
-//       body: {
-//         email: 'torrak@gmail.com',
-//         password: '123456',
-//       },
-//     }).then((response) => {
-//       // Store the session token in localStorage
-//       localStorage.setItem('sessionToken', response.body.sessionToken);
-//     });
-//   })
-// })
-
-describe('Make reservation page', () => {
+describe('US2-1 User should make a reservation with valid time', () => {
   beforeEach(() => {
     // Visit the login page and login
-    // cy.visit('/space/6621e0dd0ca3b59b54586983');
     cy.visit('/login');
     cy.get('input[name="email"]').type('torrak@gmail.com');
     cy.get('input[name="password"]').type('123456');
     cy.get('button[type="submit"]').click();
     cy.wait(2000);
+    // Visit one space page
     cy.visit('/space/662f51b9519d18088daacf91');
-    cy.wait(1000)
+    cy.wait(3000)
   });
 
-  it('No date provided', () => {
+  it('No date and time provided', () => {
     cy.contains('button', 'Reserve').click();
     cy.contains('Please provide date').should('be.visible');
   });
@@ -40,22 +24,63 @@ describe('Make reservation page', () => {
     cy.contains('The seats are fully occupied. Unable to reserve.').should('be.visible');
   });
 
-  it('Reservation made successfully', () => {
+  it('Provide date and time which endDate is less than startDate', () => {
+    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').type("10:00").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').type("9:00").type('{enter}');
+    cy.wait(2000);
+    cy.contains('button', 'Reserve').click();
+    cy.contains('The seats are fully occupied. Unable to reserve.').should('be.visible');
+  });
+
+  it('Provide date and time which endDate is equal to startDate', () => {
+    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').type("10:00").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').type("10:00").type('{enter}');
+    cy.wait(2000);
+    cy.contains('button', 'Reserve').click();
+    cy.contains('The seats are fully occupied. Unable to reserve.').should('be.visible');
+  });
+
+  it('Provide date and time which endDate is greater than startDate', () => {
     cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
     cy.get('[data-testid="spaceStartTime"]').type("10:00").type('{enter}');
     cy.get('[data-testid="spaceEndTime"]').type("11:00").type('{enter}');
     cy.wait(2000);
+    cy.contains
     cy.contains('button', 'Reserve').click();
     cy.contains('Create successfully').should('be.visible');
   });
-
-  // it('successfully makes a reservation with valid data', () => {
-    
-  // });
 })
 
-//US2-3
-describe('edit reservation', () => {
+describe('US2-2 User can view amount of available seats of selected time', () => {
+  beforeEach(() => {
+    // Visit the login page and login
+    cy.visit('/login');
+    cy.get('input[name="email"]').type('torrak@gmail.com');
+    cy.get('input[name="password"]').type('123456');
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+    // Visit one space page
+    cy.visit('/space/662f51b9519d18088daacf91');
+    cy.wait(3000)
+  });
+
+  it('TC45 logged in user can see the available time', () => {
+    cy.get('[data-testid="spaceStartTime"]').should('exist');
+    cy.get('[data-testid="spaceStartTime"]').should('exist');
+    cy.get('[data-testid="availableSeat"]').should('not.exist');
+  })
+  it('TC46 logged in user with time provided can see the amount of available seat', () => {
+    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').type("10:00").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').type("11:00").type('{enter}');
+    cy.wait(3000);
+    cy.get('[data-testid="availableSeat"]').should('exist');
+  })
+})
+
+describe('US2-3 User should edit a reservation with valid time', () => {
   beforeEach(() => {
     // Visit the login page and login
     // cy.visit('/space/6621e0dd0ca3b59b54586983');
