8dabfe66aa14e01c1b156cad70d540c3ccb1ecc7
torrak <156417791+torrak@users.noreply.github.com>
2024-04-29 15:14:26 +0700

    implement cypress

--- Frontend/cypress/e2e/epic2.cy.ts
+++ Frontend/cypress/e2e/epic2.cy.ts
@@ -25,21 +25,27 @@ describe('Make reservation page', () => {
     cy.get('input[name="password"]').type('123456');
     cy.get('button[type="submit"]').click();
     cy.wait(2000);
-    cy.visit('/space/6621e0dd0ca3b59b54586983');
+    cy.visit('/space/662f51b9519d18088daacf91');
   });
 
-  // it('No date provided', () => {
-  //   cy.contains('button', 'Reserve').click();
-  //   cy.contains('Please provide date').should('be.visible');
-  // });
-
-  it('Reserve successfully', () => {
-    // cy.contains('date-picker-input').type('01/05/2024')
-    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01");
-    cy.get('[data-testid="startTimePicker"]').type("17:00");
-    cy.get('[data-testid="endTimePicker"]').type("18:00");
+  it('No date provided', () => {
     cy.contains('button', 'Reserve').click();
     cy.contains('Please provide date').should('be.visible');
+  });
+
+  it('Provide date but no time provided', () => {
+    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
+    cy.contains('button', 'Reserve').click();
+    cy.contains('The seats are fully occupied. Unable to reserve.').should('be.visible');
+  });
+
+  it('Reservation made successfully', () => {
+    cy.get('[data-testid="spaceDatePicker"]').type("2024-05-01").type('{enter}');
+    cy.get('[data-testid="spaceStartTime"]').type("10:00").type('{enter}');
+    cy.get('[data-testid="spaceEndTime"]').type("11:00").type('{enter}');
+    cy.wait(2000);
+    cy.contains('button', 'Reserve').click();
+    cy.contains('Create successfully').should('be.visible');
   });
 
   // it('successfully makes a reservation with valid data', () => {
